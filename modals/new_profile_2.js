c1b99a3a1dd26e6b4ae6247d7d644bb6575897a2
Tavi <66774833+Coder-Tavi@users.noreply.github.com>
2022-09-07 08:23:00 -0700

    fix(profile): Add debug message for users

--- modals/new_profile_2.js
+++ modals/new_profile_2.js
@@ -1,15 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 const { Client, ModalSubmitInteraction, ModalSubmitFields } = require("discord.js");
+const { emojis } = require("../config.json");
 
 module.exports = {
   name: "new_profile_2",
   /**
    * @param {Client} client
-   * @param {ModalSubmitInteraction} interactions
+   * @param {ModalSubmitInteraction} interaction
    * @param {ModalSubmitFields} fields
    */
-  run: async (client, interaction, fields) => {
-    return [client, interaction, fields];
-    // Prevent modal from triggering anything else since it's awaited in new_profile_1.js
+  run: async (client, interaction) => {
+    await require("node:util").promisify(setTimeout)(1500);
+    if(!interaction.deferred && !interaction.replied)
+      return interaction.reply({ content: `${emojis.failure} | The modal timed out. Please try again!`, ephemeral: true, components: [] });
+    else
+      return true;
   }
 };
